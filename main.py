ce8ea39d8e4e4269eb3d0c0d5491ddcb0f31a46e
Brincoveanu Tiberiu <brincoveanutiberiu@gmail.com>
2026-02-03 21:31:47 +0200

    added a limit to the frame rate of 60 fps

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     while True:
         log_state()
@@ -19,9 +21,10 @@ def main():
 
         pygame.display.flip()
         
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
+        
+        print(dt)        
+    
 
 
 if __name__ == "__main__":
