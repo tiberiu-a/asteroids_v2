47598ac6677cf501bd89756e12505a84e64edc40
Brincoveanu Tiberiu <brincoveanutiberiu@gmail.com>
2026-02-09 17:36:40 +0200

    added player and movement for the player

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import pygame
 from logger import log_state
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
     
     while True:
         log_state()
@@ -19,9 +21,13 @@ def main():
     
         screen.fill("black")
 
+        player.update(dt)
+        player.draw(screen)
+        
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
+        
         
         print(dt)        
     
